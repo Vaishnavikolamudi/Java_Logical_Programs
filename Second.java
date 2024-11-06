d14c3c6e65c0311eece7c391182cd484aaa9bcdb
Vaishnavi Kolamudi <162945234+Vaishnavikolamudi@users.noreply.github.com>
2024-11-06 16:38:54 +0530

    Add files via upload

---
+++ Second.java
@@ -0,0 +1,14 @@
+
+class Second
+{
+   public static void main(String args[])
+   {
+      int a=10;
+      String name="vaishu";
+      Float ft=34.54F;
+
+      System.out.println(a);
+      System.out.println("The a value is:"+a);
+      System.out.println("The Given value is:"+ft);
+      }
+}
