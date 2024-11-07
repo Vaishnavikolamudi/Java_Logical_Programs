9abf3c50e8ab8eb60553ae51a46023120c1a502d
Vaishnavi Kolamudi <162945234+Vaishnavikolamudi@users.noreply.github.com>
2024-11-07 16:10:23 +0530

    Add files via upload

---
+++ Biggest2.java
@@ -0,0 +1,20 @@
+class Biggest2
+{
+    public static void main(String[] args)
+    {
+     int a=10, b=10;
+     if(a>b)
+     {
+      System.out.println("a is big");
+      }
+     else if(b>a)
+     {
+     System.out.println("b is big");
+     }
+else
+{
+System.out.println("b is big");
+}
+}
+}
+     
