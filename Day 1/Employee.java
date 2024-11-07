ef5023068d4a13a619e88eb5f153a858b53b80e3
Vaishnavi Kolamudi <162945234+Vaishnavikolamudi@users.noreply.github.com>
2024-11-07 18:01:04 +0530

    Add files via upload

---
+++ Day 1/Employee.java
@@ -0,0 +1,14 @@
+
+class Employee
+{
+   public static void main(String args[])
+   {
+      int a=101;
+      String name="vaishu";
+      Float sal=544.45F;
+
+      System.out.println("The given Employeee number is:"+a);
+      System.out.println("The given Employee name is:"+name);
+      System.out.println("The given Employee salary is:"+sal);
+      }
+}
---
+++ Day 1/First.java
@@ -0,0 +1,18 @@
+//sample program
+/*
+    sample
+    java
+    program
+*/
+import java.lang.System;
+import java.lang.String;
+
+//import java.lang.*;
+
+class First
+{
+   public static void main(String args[])
+   {
+      System.out.print("WELCOME TO FIRST JAVA PROGRAM");
+      }
+}
