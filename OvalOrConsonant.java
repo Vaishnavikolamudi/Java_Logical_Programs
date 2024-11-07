65658e4d9c8df04605e6f6254dde79da14e9f153
Vaishnavi Kolamudi <162945234+Vaishnavikolamudi@users.noreply.github.com>
2024-11-07 15:46:01 +0530

    Add files via upload

---
+++ OvalOrConsonant.java
@@ -0,0 +1,15 @@
+class OvalOrConsonant
+{
+    public static void main(String[] args)
+    {
+     char ch='d';
+     if(ch=='a'|| ch=='e'|| ch=='i'|| ch=='o'|| ch=='u')
+     {
+     System.out.println("Oval");
+     }
+     else
+     {
+     System.out.println("Consonant");
+     }
+     }
+}
