088253d92af41bb46d3e01b66308c8a23d8f61de
Vaishnavi Kolamudi <162945234+Vaishnavikolamudi@users.noreply.github.com>
2024-11-07 14:51:04 +0530

    Add files via upload

---
+++ StudentDetails.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+class StudentDetails                                                                                                                                                                                                       
+{
+  public static void main(String[] args)
+  {
+   Scanner sc=new Scanner(System.in);
+
+   int sno,m,p,c,tot;
+   String sname;
+   float savg;
+   
+   System.out.println("Enter student number");
+   sno=sc.nextInt();
+   System.out.println("Enter student name");
+   sname=sc.next();
+   System.out.println("Enter Maths subject marks");
+   m=sc.nextInt();
+   System.out.println("Enter Physics subject marks");
+   p=sc.nextInt();
+   System.out.println("Enter Chemistry subject marks");
+   c=sc.nextInt();
+   
+   tot=m+p+c;
+   savg=tot/3;
+   System.out.println("The total is:+tot");
+   System.out.println("The average is:"+savg);
+   }
+}
+   
+   
+   
+   
+
+
+   
+ 
