5cc39f9925feba825f5ebdcbba8779cee05bb1e2
Vaishnavi Kolamudi <162945234+Vaishnavikolamudi@users.noreply.github.com>
2024-11-07 15:37:34 +0530

    Add files via upload

---
+++ OddEven.java
@@ -0,0 +1,15 @@
+class OddEven
+{
+    public static void main(String[] args)
+    {
+     int a=20;
+     if(a%2==0)
+     {
+     System.out.println("The number is Odd");
+     }
+     else
+     {
+     System.out.println("The number is Even");
+     }
+     }
+}
